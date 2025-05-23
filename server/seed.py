33ac56adeff187f5e74eda11501467be031f0d0c
Tom Amollo <iantom96@gmail.com>
2025-05-23 19:01:15 +0300

    Update seed.py

--- server/seed.py
+++ server/seed.py
@@ -1,61 +1,47 @@
-# server/models.py
+#!/usr/bin/env python3
+# server/seed.py
 
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
+import datetime
+from app import app
+from models import db, Employee, Review, Onboarding
 
-convention = {
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
-}
+with app.app_context():
 
-metadata = MetaData(naming_convention=convention)
+    # Delete all rows in tables
+    Employee.query.delete()
+    Review.query.delete()
+    Onboarding.query.delete()
 
-db = SQLAlchemy(metadata=metadata)
+    # Add model instances to database
+    uri = Employee(name="Uri Lee", hire_date=datetime.datetime(2022, 5, 17))
+    tristan = Employee(name="Tristan Tal",
+                       hire_date=datetime.datetime(2020, 1, 30))
+    db.session.add_all([uri, tristan])
+    db.session.commit()
 
+    # 1..many relationship between Employee and Review
+    uri_2023 = Review(year=2023,
+                      summary="Great web developer!",
+                      employee=uri)
+    tristan_2021 = Review(year=2021,
+                          summary="Good coding skills, often late to work",
+                          employee=tristan)
+    tristan_2022 = Review(year=2022,
+                          summary="Strong coding skills, takes long lunches",
+                          employee=tristan)
+    tristan_2023 = Review(year=2023,
+                          summary="Awesome coding skills, dedicated worker",
+                          employee=tristan)
+    db.session.add_all([uri_2023, tristan_2021, tristan_2022, tristan_2023])
+    db.session.commit()
 
-class Zookeeper(db.Model, SerializerMixin):
-    __tablename__ = 'zookeepers'
-
-    # don't forget that every tuple needs at least one comma!
-    serialize_rules = ('-animals.zookeeper',)
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
-    birthday = db.Column(db.Date)
-
-    animals = db.relationship('Animal', back_populates='zookeeper')
-
-
-class Enclosure(db.Model, SerializerMixin):
-    __tablename__ = 'enclosures'
-
-    serialize_rules = ('-animals.enclosure',)
-
-    id = db.Column(db.Integer, primary_key=True)
-    environment = db.Column(db.String)
-    open_to_visitors = db.Column(db.Boolean)
-
-    animals = db.relationship('Animal', back_populates='enclosure')
-
-
-class Animal(db.Model, SerializerMixin):
-    __tablename__ = 'animals'
-
-    serialize_rules = ('-zookeeper.animals', '-enclosure.animals',)
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
-    species = db.Column(db.String)
-
-    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
-    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
-
-    enclosure = db.relationship('Enclosure', back_populates='animals')
-    zookeeper = db.relationship('Zookeeper', back_populates='animals')
-
-    def __repr__(self):
-        return f'<Animal {self.name}, a {self.species}>'
+    # 1..1 relationship between Employee and Onboarding
+    uri_onboarding = Onboarding(
+        orientation=datetime.datetime(2023, 3, 27),
+        employee=uri)
+    tristan_onboarding = Onboarding(
+        orientation=datetime.datetime(2020, 1, 20, 14, 30),
+        forms_complete=True,
+        employee=tristan)
+    db.session.add_all([uri_onboarding, tristan_onboarding])
+    db.session.commit()
